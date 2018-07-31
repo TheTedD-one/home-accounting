4efed1313e3c8a3e7fdbb568a21116feae966c3a
TheTedD_luxystech <r.khabitov@luxystech.com>
2018-07-31 16:13:42 +0600

    Moment pipe

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -13,6 +13,7 @@ import {DropdownDirective} from './shared/directives/dropdown.directive';
 import { BillCardComponent } from './bill-page/bill-card/bill-card.component';
 import { CurrencyCardComponent } from './bill-page/currency-card/currency-card.component';
 import {BillService} from './shared/services/bill.service';
+import {MomentPipe} from './shared/pipes/moment.pipe';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import {BillService} from './shared/services/bill.service';
     HeaderComponent,
     DropdownDirective,
     BillCardComponent,
-    CurrencyCardComponent
+    CurrencyCardComponent,
+    MomentPipe
   ],
   imports: [
     CommonModule,
