eb1e286457f15c5dc66f1279792391aa6a59964b
TheTedD_luxystech <r.khabitov@luxystech.com>
2018-08-01 10:40:08 +0600

    edit-category

--- src/app/system/records-page/edit-category/edit-category.component.ts
+++ src/app/system/records-page/edit-category/edit-category.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {Category} from '../../shared/models/category.model';
+import {Observable} from 'rxjs';
+import {CategoriesService} from '../../shared/services/categories.service';
+import {Message} from '../../../shared/models/message.model';
 
 @Component({
   selector: 'ha-edit-category',
@@ -7,9 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditCategoryComponent implements OnInit {
 
-  constructor() { }
+  @Input() categories: Category[] = [];
+  @Output() onCategoryEdit = new EventEmitter<Category>();
+
+  currentCategoryId = 1;
+  currentCategory: Category;
+  message: Message;
+
+  constructor(private categoriesService: CategoriesService) { }
 
   ngOnInit() {
+    this.message = new Message('success', '');
+    this.onCategoryChange();
   }
+
+  onSubmit(form: NgForm) {
+    const {name} = form.value;
+    let {capacity} = form.value;
+    if (capacity < 0) {
+      capacity *= -1;
+    }
+
+    const category = new Category(name, capacity, +this.currentCategoryId);
+    this.categoriesService.updateCategory(category)
+      .subscribe((data: Category) => {
+        this.onCategoryEdit.emit(data);
+        this.message.text = 'Категория успешно отредактирована.';
+        window.setTimeout(() => this.message.text = '', 5000);
+      });
+  }
+
+  onCategoryChange() {
+    this.currentCategory = this.categories
+      .find(c => c.id === +this.currentCategoryId);
+  }
+
+
 
 }
