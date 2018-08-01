dd11dedee17bbcd7dad8f44b99ae13bf63cd408b
TheTedD <thetedd.one@gmail.com>
2018-08-01 20:33:22 +0600

    history filter-pipe

--- src/app/system/history-page/history-events/history-events.component.ts
+++ src/app/system/history-page/history-events/history-events.component.ts
@@ -12,6 +12,10 @@ export class HistoryEventsComponent implements OnInit {
   @Input() categories: Category[] = [];
   @Input() events: HAEvent[] = [];
 
+  searchValue = '';
+  searchPlaceholder = 'Сумма';
+  searchField = 'amount';
+
   constructor() { }
 
   ngOnInit() {
@@ -25,6 +29,18 @@ export class HistoryEventsComponent implements OnInit {
       'label': true,
       'label-danger': event.type === 'outcome',
       'label-success': event.type === 'income',
-    }
+    };
+  }
+
+  changeCriteria(field: string) {
+    const namesMap = {
+      amount: 'Сумма',
+      date: 'Дата',
+      category: 'Категория',
+      type: 'Тип'
+    };
+
+    this.searchPlaceholder = namesMap[field];
+    this.searchField = field;
   }
 }
